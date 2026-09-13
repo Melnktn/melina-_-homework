f9dea62843f2ea547b14500c826b3517b34102f0
Melina <kmelina403@gmail.com>
2026-09-13 13:41:12 -0700

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+# hoghoogh = int(input("hoghoogh :"))
+
+# maliyat = int(input("maliyat :"))
+
+# pardakhti = hoghoogh - maliyat
+
+# print(pardakhti , "hoghooghepardakhti :")
+
+
+
+
+
+# username = "melina"
+
+# print((username + "\n") *10)
+
+
+
+
+username = "melina"
+
+print(username * 10)
+
+
+
+
+
+
+
+username = ("""melina rider with kawasaki """)
+
+number = 10
+
+total = username * number
+
+print(total)
